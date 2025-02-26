95b998a696cf78737f91634e068ef9c59597f7c9
C4ffuLatte <tebithegamer@gmail.com>
2025-02-26 15:21:02 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -661,15 +661,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
 Cal("init", "30min", {origin:"https://cal.com"});
-
-Cal.ns["30min"]("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
   
-  // Important: Please add the following attributes to the element that should trigger the calendar to open upon clicking.
-  // data-cal-link="vladimyr-9cyji1/30min"
-  // data-cal-namespace="30min"
-  // data-cal-config='{"layout":"month_view"}'
+// Important: Please add the following attributes to the element that should trigger the calendar to open upon clicking.
+// `data-cal-link="fg-cabahug-trading/30min"`
+// data-cal-namespace="30min"
+// `data-cal-config='{"layout":"month_view"}'`
+Cal.ns["30min"]("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
 
-  Cal.ns["30min"]("ui", {"cssVarsPerTheme":{"light":{"cal-brand":"#004E1F"}},"hideEventTypeDetails":false,"layout":"month_view"});
 
   document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('imageModal');
